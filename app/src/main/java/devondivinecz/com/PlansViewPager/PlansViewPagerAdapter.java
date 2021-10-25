64a96ccc659cda6bbebb6e287ec76cdac11361a0
Devon Divinecz <w0723299@myscc.ca>
2021-10-25 03:17:51 -0400

    Started Workout Plans Layout, Added title parameter in PlansViewPagerAdapter. Tested and works.

--- app/src/main/java/devondivinecz/com/PlansViewPager/PlansViewPagerAdapter.java
+++ app/src/main/java/devondivinecz/com/PlansViewPager/PlansViewPagerAdapter.java
@@ -13,19 +13,20 @@ public class PlansViewPagerAdapter extends FragmentStateAdapter {
     @NonNull
     @Override
     public Fragment createFragment(int position) {
-        position = position % 5;
+        position = position % 6;
         switch(position) {
-            case 0: return PlansLayoutFragment.newInstance(null, null);
-            case 1: return PlansLayoutFragment.newInstance(null, null);
-            case 2: return PlansLayoutFragment.newInstance(null, null);
-            case 3: return PlansLayoutFragment.newInstance(null, null);
-            case 4: return PlansLayoutFragment.newInstance(null, null);
+            case 0: return PlansLayoutFragment.newInstance("Bicep Plan", null);
+            case 1: return PlansLayoutFragment.newInstance("Tricep Plan", null);
+            case 2: return PlansLayoutFragment.newInstance("Shoulder Plan", null);
+            case 3: return PlansLayoutFragment.newInstance("Trap Plan", null);
+            case 4: return PlansLayoutFragment.newInstance("Forearm Plan", null);
+            case 5: return PlansLayoutFragment.newInstance("Legs Plan", null);
             default: return PlansLayoutFragment.newInstance("Fragment Not Found", null);
         }
     }
 
     @Override
     public int getItemCount() {
-        return 5;
+        return 6;
     }
 }
